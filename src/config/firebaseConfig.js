879a0971884ccd643f1e9e23d2795afe1ebf72bb
afridi21 <abdullahalafridi5@gmail.com>
2023-06-04 20:08:21 +0600

    first commit

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDV18IkRQ_W_RS8bSzdY5DwGKBJqxYjngs",
+    authDomain: "cryptoh-eea94.firebaseapp.com",
+    projectId: "cryptoh-eea94",
+    storageBucket: "cryptoh-eea94.appspot.com",
+    messagingSenderId: "1034663515910",
+    appId: "1:1034663515910:web:e5f3f03a17a4eb20597087",
+    measurementId: "G-G9G3K6XL0V"
+  };
+  export default firebaseConfig;
